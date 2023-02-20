3c4fe2346830fdfa0c6971048918e12adb0c8d85
Wazzupek01 <wazzupekgaming@gmail.com>
2023-02-20 14:10:51 +0100

    complete assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card';
+import Container from './components/Container';
+import qrCode from './images/image-qr-code.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Card 
+      title="Improve your front-end skills by building projects" 
+      description="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+      image={qrCode}
+      alt="QR Code"
+      />
+    </Container>
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,15 @@
-import React from 'react';
 import classes from './Card.module.css';
 
 const Card = (props) => {
     return (
     <div className={classes["card-wrapper"]}>
-        <div className={classes["card-image"]}><img src={qrCode} /></div>
+        <div className={classes["card-image"]}><img src={props.image} alt={props.alt}/></div>
         <div className={classes.text}>
             <h2 className={classes["card-header"]}>
                 {props.title}    
             </h2>
             <p className={classes.description}>
-                Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+                {props.description}
             </p>
         </div>
     </div>);
