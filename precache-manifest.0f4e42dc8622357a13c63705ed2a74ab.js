01a74c3dc83d0c3df7a30e707530f102da1b231a
Wazzupek01 <wazzupekgaming@gmail.com>
2023-02-20 15:27:16 +0100

    Updates

---
+++ precache-manifest.0f4e42dc8622357a13c63705ed2a74ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8a98c01f1cd2b84e533",
+    "url": "/qr-code-card-component-reactjs/static/css/main.2eca39f1.chunk.css"
+  },
+  {
+    "revision": "c8a98c01f1cd2b84e533",
+    "url": "/qr-code-card-component-reactjs/static/js/main.c8a98c01.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/qr-code-card-component-reactjs/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "b411e204f245bdc71b46",
+    "url": "/qr-code-card-component-reactjs/static/js/2.b411e204.chunk.js"
+  },
+  {
+    "revision": "2eb78e1c4eade297a1ea",
+    "url": "/qr-code-card-component-reactjs/static/js/runtime~main.2eb78e1c.js"
+  },
+  {
+    "revision": "a75b5156ab9da2eea1f0ef8e9775f0f5",
+    "url": "/qr-code-card-component-reactjs/index.html"
+  }
+];
